e87bd9f26d7457fe16d140b4413a727d826ade5a
morenamt <onalepeloo@live.com>
2020-11-02 16:27:40 +0800

    All Commits in one, ey i am still trying to get a hang of this

--- classes/dbh.class.php
+++ classes/dbh.class.php
@@ -1,36 +1,16 @@
 <?php
 
-require_once $_SERVER['DOCUMENT_ROOT']. ('/oop_pdo_crud/vendor/autoload.php');
+require './vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__. '/../');
 $dotenv->load();
 
 class Dbh {
 
-    private $host = "localhost";
-    private $user= "root";
-    private $pwd = "";
-    private $dbName= "oopblog";
-
-
-    /*
-    private $host = $_ENV["HOST"];
-    private $user= $_ENV["DB_USER"];
-    private $pwd = $_ENV["DB_PASSWORD"];
-    private $dbName= $_ENV["DB"];
-
-    public function __construct($host,$user,$pwd,$dbName){
-
-      $this->host = $host;
-      $this->user = $user;
-      $this->pwd = $pwd;
-      $this->dbName = $dbName;
-    }
-  */
     public function connect() {
         try{
 
-          $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbName;
-          $conn = new PDO($dsn, $this->user, $this->pwd);
+          $dsn = 'mysql:host=' . $_ENV["HOST"] . ';dbname=' . $_ENV["DB"];
+          $conn = new PDO($dsn, $_ENV["DB_USER"], $_ENV["DB_PASSWORD"]);
           $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
           return $conn;
 
